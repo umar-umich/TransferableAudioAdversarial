caaa0cb079ff4feba3890a41d04c48f6348fda20
devloverumar <softwareengineerumar@gmail.com>
2024-12-06 09:45:46 -0500

    transcription models added for training

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from sklearn.metrics import *
 from generator import Generator
 from discriminator import Discriminator
 # from compose_models import get_rawnet3
-from compose_models import RawNetWithFC, get_inc_ssdnet, get_rawnet2, get_ssdnet, get_wav2vec2_model
+from compose_models import RawNetWithFC, get_inc_ssdnet, get_rawnet2, get_speech_to_text_model, get_ssdnet, get_wav2vec2_model
 from generator_simple import GeneratorSimple
 from discriminator_simple import DiscriminatorSimple
 from data_loader import DATAReader
@@ -25,7 +25,7 @@ from models.rawboost.RawBoost import RawNet  # From RawBoost Repo
 from models.rawnet.RawNetBasicBlock import Bottle2neck
 from models.rsm1d.RSM1D import SSDNet1D
 from models.rawnet.RawNet3 import RawNet3
-from utils import batch_audio_to_mel, batch_mel_to_audio, get_transciption_loss, transcribe_audio
+from utils import batch_audio_to_mel, batch_mel_to_audio, get_transciption_loss, transcribe_audio, transcribe_s2t
 from visualize import compare_audio_samples
 from tqdm import tqdm
 import datetime
@@ -36,11 +36,13 @@ import yaml
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--root', '-rt', type=str, default='../DATASETS/DTIM', help='')
-parser.add_argument('--nEpochs', '-epoch', type=int, default=30, help='')
+parser.add_argument('--nEpochs', '-epoch', type=int, default=50, help='')
 parser.add_argument('--batch_size', '-b', type=int, default=16, help='')
 parser.add_argument('--num_workers', '-w', type=int, default=16, help='')
 parser.add_argument('--lr', '-lr', type=float, default=0.001, help='')
 parser.add_argument("--gpu_devices", type=int, nargs='+', default=[0], help='')
+parser.add_argument('--save_output', action='store_true', 
+                    help="Flag to save the models/output to files for future use")
 args = parser.parse_args()
 print(args)
 
@@ -83,21 +85,20 @@ optimizer_D = torch.optim.SGD(D.parameters(), lr = args.lr)
 
 scheduler_G = StepLR(optimizer_G, step_size=10, gamma=0.9)
 scheduler_D = StepLR(optimizer_D, step_size=10, gamma=0.9)
-s1_w = 0.01
+s1_w = 0.0001
 s2_w = 0.0001
-t1_weight = 10
+# s3_w = 1   # 0.0001
+t1_w = 5
+t2_w = 5
 # t2_we
-model_name = 'rawnet2'
-cl_model1 = get_rawnet2(device)
-# cl_model2 = get_ssdnet(device)
+model_name = 'ssdnet_inc_ssdnet'
+cl_model1 = get_ssdnet(device)
+cl_model2 = get_inc_ssdnet(device)
+cl_model3 = get_rawnet2(device)
 t_processor_1, t_model_1 = get_wav2vec2_model(device)
-save_dir_path = f'{save_dir_path}_{model_name}_{s1_w}_{s2_w}_{t1_weight}'
+t_processor_2, t_model_2 = get_speech_to_text_model(device)
+save_dir_path = f'{save_dir_path}_{model_name}_{s1_w}_{s2_w}_{t1_w}_{t2_w}'
 
-wav_dir_path = 'Wav_Plot_'+save_dir_path
-os.makedirs(wav_dir_path, exist_ok=True)
-
-checkpoint_dir_path = 'CHECKPOINTS_'+save_dir_path
-os.makedirs(checkpoint_dir_path, exist_ok=True)
 
 scaler = torch.GradScaler(device)
 
@@ -116,9 +117,11 @@ def sLoss(x, y, model):
 
 def train(epoch):
     g_losses = []
-    c_losses = []
+    c1_losses = []
+    c2_losses = []
     d_losses = []
-    t_losses = []
+    t1_losses = []
+    t2_losses = []
 
     progress_bar = tqdm(train_loader, desc=f"Epoch {epoch}", unit="batch", leave=True)
 
@@ -126,8 +129,8 @@ def train(epoch):
         real = train_sample[0].unsqueeze(1).to(device, dtype=torch.float)
         forged = train_sample[2].unsqueeze(1).to(device, dtype=torch.float)
 
-        y_real = torch.ones(real.shape[0]).to(device, dtype=torch.float)
-        y_fake = torch.zeros(forged.shape[0]).to(device, dtype=torch.float)
+        y_real = torch.zeros(real.shape[0]).to(device, dtype=torch.float)
+        y_fake = torch.ones(forged.shape[0]).to(device, dtype=torch.float)
 
         # ========================Generator==============================
         optimizer_G.zero_grad()
@@ -135,27 +138,34 @@ def train(epoch):
         with torch.autocast(device_type='cuda', dtype=torch.float16):  # Enable Mixed Precision
             attacked = G(forged)
 
-            forged_transciption = transcribe_audio(forged,t_processor_1,t_model_1,device)
-            attacked_transciption = transcribe_audio(attacked,t_processor_1,t_model_1,device)
-            if index == 0:
+            forged_transciption1 = transcribe_audio(forged,t_processor_1,t_model_1,device)
+            attacked_transciption1 = transcribe_audio(attacked,t_processor_1,t_model_1,device)
+
+            forged_transciption2 = transcribe_s2t(forged,t_processor_2,t_model_2,device)
+            attacked_transciption2 = transcribe_s2t(attacked,t_processor_2,t_model_2,device)
+
+            if index == 0 and args.save_output:
                 forged_audio = forged[0].detach()  # Select first sample of forged audio
                 attacked_audio = attacked[0].detach()   # Corresponding generated audio
 
-                forged_t_samples = forged_transciption
-                attacked_t_samples = attacked_transciption
-
                 # Plot and compare
-                compare_audio_samples(forged_audio, attacked_audio, forged_t_samples, attacked_t_samples,epoch,wav_dir_path, sr=16000)
+                wav_dir_path = 'Wav_Plot_'+save_dir_path
+                os.makedirs(wav_dir_path, exist_ok=True)
+                compare_audio_samples(forged_audio, attacked_audio, forged_transciption1, attacked_transciption1, forged_transciption2, attacked_transciption2,epoch,wav_dir_path, sr=16000)
 
 
-            t_loss = get_transciption_loss(forged_transciption,attacked_transciption)
+            t1_loss = get_transciption_loss(forged_transciption1,attacked_transciption1)
+            t2_loss = get_transciption_loss(forged_transciption2,attacked_transciption2)
 
             per_loss = perceptual_loss(forged, attacked)
             adv_loss = adversarial_loss(y_real, D(attacked).squeeze().detach())
             c1_loss = sLoss(attacked, y_real.to(dtype=torch.long),cl_model1)
-            # c2_loss = sLoss(attacked, y_real.to(dtype=torch.long),cl_model2)
+            c2_loss = sLoss(attacked, y_real.to(dtype=torch.long),cl_model2)
+            # c3_loss = sLoss(attacked.squeeze(1), y_fake.to(dtype=torch.long),cl_model3)   # used fake label just to revert the label  for rawnet
+            # c3_loss = np.float32(c3_loss.item())   # required for rawnet_2
 
-            g_loss = per_loss + adv_loss + s1_w * c1_loss+ t1_weight*t_loss  # s2_w*c2_loss + 
+            print(f"C losses: {c1_loss}, {c2_loss}, T1 loss: {t1_loss}, T2 loss: {t2_loss}")
+            g_loss = per_loss + adv_loss + s1_w*c1_loss+ s2_w*c2_loss + t1_w*t1_loss + t2_w*t2_loss  # s2_w*c2_loss + 
 
         # Scale the loss and backpropagate
         scaler.scale(g_loss).backward()
@@ -175,20 +185,24 @@ def train(epoch):
         scaler.update()
 
         g_losses.append(g_loss.item())
-        c_losses.append(c1_loss.item())  # +c2_loss.item()
+        c1_losses.append(c1_loss.item())  # +c2_loss.item()
+        c2_losses.append(c2_loss.item())  # +c2_loss.item()
         d_losses.append(d_loss.detach().cpu().numpy())
-        t_losses.append(t_loss.item())
+        t1_losses.append(t1_loss.item())
+        t2_losses.append(t2_loss.item())
 
         # Update tqdm progress bar with current losses
         progress_bar.set_postfix({
             "G_Loss": f"{np.mean(g_losses):.4f}",
-            "C_Loss": f"{np.mean(c_losses):.4f}",
+            "C1_Loss": f"{np.mean(c1_losses):.4f}",
+            "C2_Loss": f"{np.mean(c2_losses):.4f}",
             "D_Loss": f"{np.mean(d_losses):.4f}",
-            "T_Loss": f"{np.mean(t_losses):.4f}",
+            "T1_Loss": f"{np.mean(t1_losses):.4f}",
+            "T2_Loss": f"{np.mean(t2_losses):.4f}",
         })
 
     progress_bar.close()  # Ensure tqdm closes cleanly when done
-    return np.mean(g_losses), np.mean(c_losses), np.mean(d_losses), np.mean(t_losses)
+    return np.mean(g_losses), np.mean(c1_losses),np.mean(c2_losses), np.mean(d_losses), np.mean(t1_losses), np.mean(t2_losses)
 
 def cal_acc(y, x, model):
     # outputs = inception(x)
@@ -203,8 +217,11 @@ def cal_acc(y, x, model):
 
 def test(epoch=0):
     G.eval()
-    real_acc, fake_acc, af_acc = [], [], []
-    real2_acc,fake2_acc,af2_acc = [], [], []
+    accuracies = {
+        "cl_model1": {"real": [], "fake": [], "af": []},
+        "cl_model2": {"real": [], "fake": [], "af": []},
+        # "cl_model3": {"real": [], "fake": [], "af": []},
+    }
 
     progress_bar = tqdm(test_loader, desc=f"[Test] Epoch {epoch}", unit="batch", leave=True)
 
@@ -212,54 +229,80 @@ def test(epoch=0):
         real = test_sample[0].unsqueeze(1).to(device, dtype=torch.float)
         forged = test_sample[2].unsqueeze(1).to(device, dtype=torch.float)
 
-        y_real = torch.ones(real.shape[0]).to(device, dtype=torch.float)
-        y_fake = torch.zeros(forged.shape[0]).to(device, dtype=torch.float)
+        y_real = torch.zeros(real.shape[0]).to(device, dtype=torch.float)
+        y_fake = torch.ones(forged.shape[0]).to(device, dtype=torch.float)
 
         with torch.autocast(device_type='cuda', dtype=torch.float16):  # Enable Mixed Precision
             fake = G(forged)
 
-            real_acc.append(cal_acc(y_real, real,cl_model1))
-            fake_acc.append(cal_acc(y_fake, forged,cl_model1))
-            af_acc.append(cal_acc(y_fake, fake,cl_model1))
+            for model_name, model in zip(
+                ["cl_model1", "cl_model2"], [cl_model1, cl_model2]
+            ):
+                
+                if model_name in 'cl_model3':
+                    accuracies[model_name]["real"].append(cal_acc(y_fake, real.squeeze(1), model))          # used fake label just to revert the label  for rawnet
+                    accuracies[model_name]["fake"].append(cal_acc(y_real, forged.squeeze(1), model))   
+                    accuracies[model_name]["af"].append(cal_acc(y_real, fake.squeeze(1), model))
+                else:
+                    accuracies[model_name]["real"].append(cal_acc(y_real, real, model))
+                    accuracies[model_name]["fake"].append(cal_acc(y_fake, forged, model))
+                    accuracies[model_name]["af"].append(cal_acc(y_fake, fake, model))
 
-
-            # real2_acc.append(cal_acc(y_real, real,cl_model2))
-            # fake2_acc.append(cal_acc(y_fake, forged,cl_model2))
-            # af2_acc.append(cal_acc(y_fake, fake,cl_model2))
-            
     progress_bar.close()
-    return 100 * np.mean(real_acc), 100 * np.mean(fake_acc), 100 * np.mean(af_acc) #, 100 * np.mean(real2_acc), 100 * np.mean(fake2_acc), 100 * np.mean(af2_acc) 
+
+    # Compute mean accuracies
+    results = {
+        model_name: {
+            metric: 100 * np.mean(values) for metric, values in acc_dict.items()
+        }
+        for model_name, acc_dict in accuracies.items()
+    }
+
+    return results
+
 
 
 def main():
     print('Training on', 2*len(train_dataset), 'and validating on ', 2*len(test_dataset), 'samples.')
 
     for epoch in range(args.nEpochs):
-        g_loss, c_loss, d_loss, t_loss = train(epoch)
+        g_loss, c1_loss,c2_loss, d_loss, t1_loss, t2_loss = train(epoch)
 
         scheduler_G.step()
         scheduler_D.step()
 
-        print("[Train] [Epoch %d/%d], [LR: G=%f, D=%f], [C loss: %f], [D loss: %f], [G loss: %f], [T loss: %f]" % (
-            epoch, args.nEpochs, scheduler_G.get_last_lr()[0],  scheduler_D.get_last_lr()[0], c_loss, d_loss, g_loss, t_loss))
+        print("[Train] [Epoch %d/%d], [LR: G=%f, D=%f], [C1 loss: %f], [C2 loss: %f], [D loss: %f], [G loss: %f], [T1 loss: %f], [T2 loss: %f]" % (
+            epoch, args.nEpochs, scheduler_G.get_last_lr()[0],  scheduler_D.get_last_lr()[0], c1_loss, c2_loss, d_loss, g_loss, t1_loss,t2_loss))
 
-        checkpoints = {
-            'epoch': epoch+1,
-            'state_dict': G.state_dict()
-        }
 
-        # Save the checkpoint
-        torch.save(checkpoints, osp.join(checkpoint_dir_path, f'generator_{epoch+1}.pth'))
+        results = test(epoch)
 
-        r_acc, f_acc, af_acc = test(epoch) # , r2_acc, f2_acc, af2_acc
+        # Extract results for each classifier
+        r_acc, f_acc, af_acc = results["cl_model1"].values()
+        r2_acc, f2_acc, af2_acc = results["cl_model2"].values()
+        # r3_acc, f3_acc, af3_acc = results["cl_model3"].values()
 
-        print('[Test Cl_1] [Epoch %d/%d], [Acc: %.2f, %.2f, %.2f]'% (epoch, args.nEpochs, r_acc, f_acc, af_acc))
-        # print('[Test Cl_2] [Epoch %d/%d], [Acc: %.2f, %.2f, %.2f]'% (epoch, args.nEpochs, r2_acc, f2_acc, af2_acc))
-        # Save test accuracies to a file
-        results_file = osp.join(checkpoint_dir_path, 'test_accuracies.txt')
-        with open(results_file, 'a') as f:
-            f.write(f"Cl1: Epoch {epoch + 1}/{args.nEpochs}: Acc=({r_acc:.2f}, {f_acc:.2f}, {af_acc:.2f})\n")
-            # f.write(f"Cl2: Epoch {epoch + 1}/{args.nEpochs}: Acc=({r2_acc:.2f}, {f2_acc:.2f}, {af2_acc:.2f})\n")
+        # Print the results
+        print('[Test Cl_1] [Epoch %d/%d], [Acc: %.2f, %.2f, %.2f]' % (epoch, args.nEpochs, r_acc, f_acc, af_acc))
+        print('[Test Cl_2] [Epoch %d/%d], [Acc: %.2f, %.2f, %.2f]' % (epoch, args.nEpochs, r2_acc, f2_acc, af2_acc))
+        # print('[Test Cl_3] [Epoch %d/%d], [Acc: %.2f, %.2f, %.2f]' % (epoch, args.nEpochs, r3_acc, f3_acc, af3_acc))
+
+        if args.save_output:
+            checkpoints = {
+                'epoch': epoch+1,
+                'state_dict': G.state_dict()
+            }
+            
+            checkpoint_dir_path = 'CHECKPOINTS_'+save_dir_path
+            os.makedirs(checkpoint_dir_path, exist_ok=True)
+            # Save the checkpoint
+            torch.save(checkpoints, osp.join(checkpoint_dir_path, f'generator_{epoch+1}.pth'))
+
+            # Save test accuracies to a file
+            results_file = osp.join(checkpoint_dir_path, 'test_accuracies.txt')
+            with open(results_file, 'a') as f:
+                f.write(f"Cl1: Epoch {epoch + 1}/{args.nEpochs}: Acc=({r_acc:.2f}, {f_acc:.2f}, {af_acc:.2f}),\t\t")
+                f.write(f"Cl2: Epoch {epoch + 1}/{args.nEpochs}: Acc=({r2_acc:.2f}, {f2_acc:.2f}, {af2_acc:.2f})\n\n")
 
 if __name__ == '__main__':
     main()
