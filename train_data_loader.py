b38943fa46b1e6d46a4ba84008907099f2a42ac7
devloverumar <softwareengineerumar@gmail.com>
2025-05-09 12:38:55 -0400

    Training data code Feb 2024

--- train_data_loader.py
+++ train_data_loader.py
@@ -4,25 +4,86 @@ import os
 import os.path as osp
 import numpy as np
 from glob import glob
+import csv
 
-from data_loader import get_labels, load_preprocess_AASIST
+# from data_loader import get_labels, load_preprocess_AASIST
+
+def get_labels(labels_file):
+    labels = {}
+    with open(labels_file, 'r') as file:
+        for line in file:
+            parts = line.strip().split(' ')
+            if len(parts) >= 5:  # Assuming the line structure is consistent
+                file_id = parts[1].strip()  # Extract the file_id
+                label = parts[-1].strip()   # Extract the label (e.g., "spoof" or "bonafide")
+                labels[file_id] = 1 if label == 'spoof' else 0  # 1 for fake, 0 for real
+    return labels
+
+
+def get_in_the_wild_labels(labels_file):
+    labels = {}
+    with open(labels_file, mode='r', newline='', encoding='utf-8') as file:
+        reader = csv.DictReader(file, delimiter=',')  # Assuming tab-separated CSV, adjust delimiter if needed
+        # print("CSV Headers:", reader.fieldnames)  # This will show you the headers of the CSV
+
+        for row in reader:
+            file_id = os.path.splitext(row['file'].strip())[0]  # Strip extension from 'file' (e.g., '0.wav' -> '0')
+            label = row['label'].strip()   # Extract the label (e.g., 'spoof' or 'bona-fide')
+            
+            # Map the label to 1 (spoof) or 0 (bona-fide)
+            labels[file_id] = 1 if label == 'spoof' else 0
+    
+    return labels
+
+
+def load_preprocess_AASIST(path, cut=96000):   # 96000, 64600
+    from torch import Tensor
+    import librosa
+
+
+    # X, _ = sf.read(path)
+    X, _ = librosa.load(path, sr=16000, mono=True)  # Record_1.mp3    Derek_orig_1.wav
+    X_pad = pad(X, cut)
+    x_inp = Tensor(X_pad)
+    if len(x_inp.shape) != 1:
+        if x_inp.shape[-1] != 1:
+            x_inp = x_inp.mean(dim=-1, keepdim=False)
+    return x_inp
+
+
+def pad(x, max_len=64600):
+    x_len = x.shape[0]
+    if x_len >= max_len:
+        return x[:max_len]
+    # need to pad
+    num_repeats = int(max_len / x_len) + 1
+    padded_x = np.tile(x, (1, num_repeats))[:, :max_len][0]
+    return padded_x
 
 
 class RawNetDATAReader(data.Dataset):
     def __init__(self, args=None, split=None, labels=None):
         self.args = args
         if split == 'TRAIN':
-            labels_file = '/data/Umar/A_Datasets/ASV_2019/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.train.trl.txt'
+            labels_file = '/data/Shared_Audio/A_Datasets/ASV_2019/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.train.trl.txt'
             labels = get_labels(labels_file)
-            self.data_root = '/data/Umar/A_Datasets/ASV_2019/ASVspoof2019_LA_train/flac'
+            self.data_root = '/data/Shared_Audio/A_Datasets/ASV_2019/ASVspoof2019_LA_train/flac'
         elif split == 'TEST':
-            labels_file = '/data/Umar/A_Datasets/ASV_2019/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.eval.trl.txt'
+            labels_file = '/data/Shared_Audio/A_Datasets/ASV_2019/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.eval.trl.txt'
             labels = get_labels(labels_file)
-            self.data_root = '/data/Umar/A_Datasets/ASV_2019/ASVspoof2019_LA_eval/flac'
+            self.data_root = '/data/Shared_Audio/A_Datasets/ASV_2019/ASVspoof2019_LA_eval/flac'
         elif split == 'DEV':
-            labels_file = '/data/Umar/A_Datasets/ASV_2019/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.dev.trl.txt'
+            labels_file = '/data/Shared_Audio/A_Datasets/ASV_2019/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.dev.trl.txt'
             labels = get_labels(labels_file)
-            self.data_root = '/data/Umar/A_Datasets/ASV_2019/ASVspoof2019_LA_dev/flac'
+            self.data_root = '/data/Shared_Audio/A_Datasets/ASV_2019/ASVspoof2019_LA_dev/flac'
+        elif split in 'In_The_Wild':
+            dev_labels_file = '/data/Shared_Audio/A_Datasets/release_in_the_wild/meta.csv'
+            labels = get_in_the_wild_labels(dev_labels_file)
+            self.data_root = '/data/Shared_Audio/A_Datasets/release_in_the_wild'
+        elif split in 'WaveFake':
+            # dev_labels_file = '/data/Shared_Audio/A_Datasets/release_in_the_wild/meta.csv'
+            # labels = get_in_the_wild_labels(dev_labels_file)
+            self.data_root = '/data/Shared_Audio/A_Datasets/WaveFake/ljspeech_hifiGAN'
 
         self.labels = labels  # Provided labels for real/fake classification
 
@@ -69,7 +130,7 @@ class RawNetDATAReader(data.Dataset):
         dataset_path = self.data_root
 
         for file_name in os.listdir(dataset_path):
-            if file_name.endswith('.flac'):  # Audio files of interest
+            if file_name.endswith('.flac') or file_name.endswith('.wav'):  # Audio files of interest
                 file_path = osp.join(dataset_path, file_name)
 
                 # Check label (real=0, fake=1)
--- train_surrogate.py
+++ train_surrogate.py
@@ -12,6 +12,7 @@ from models.rawboost.RawBoost import RawNet
 from models.rawnet.RawNet2 import RawNet2
 from models.resnet1d.multi_scale_ori import MSResNet
 from models.resnet1d.resnet1d import ResNet1D
+from models.rsm1d.RSM1D import DilatedNet, SSDNet1D
 from models.rsm1d.RSM1D_large import DilatedNet_L, SSDNet1D_L
 from models.rsm1d.RSM1D_small import DilatedNet_S, SSDNet1D_S
 import torch
@@ -23,6 +24,7 @@ from sklearn.metrics import roc_auc_score, roc_curve
 import numpy as np
 
 from train_data_loader import RawNetDATAReader
+from train_data_loader_combined import CombinedDATAReader
 
 
 
@@ -117,11 +119,11 @@ def train_model(model, criterion, optimizer , train_loader, val_loader, num_epoc
 
             with torch.autocast(device_type='cuda', dtype=torch.float16):  # Mixed precision forward pass
                 # Forward pass
-                print(f"Input Size: {inputs.shape}")
+                # print(f"Input Size: {inputs.shape}")
                 outputs = model(inputs)
                 # outputs = outputs[1]         # outputs[1] for aassit
-                print(f"Output shape: {outputs.shape}")
-                print(f"Labels shape: {labels.shape}")
+                # print(f"Output shape: {outputs.shape}")
+                # print(f"Labels shape: {labels.shape}")
 
                 loss = criterion(outputs, labels)      
             # Scaled backward pass
@@ -146,10 +148,10 @@ def train_model(model, criterion, optimizer , train_loader, val_loader, num_epoc
         # Evaluate on the validation set
         val_accuracy, val_loss, auc, eer = evaluate_model(model, criterion, val_loader)
         print(f"Epoch {epoch+1}/{num_epochs} - Loss: {epoch_loss:.4f} - Accuracy: {100 * correct / total:.2f}% - "
-              f"Val Accuracy: {val_accuracy:.2f}% - Val Loss: {val_loss:.4f} - AUC: {auc:.4f} - EER: {eer:.4f}")
+              f"Val Accuracy: {val_accuracy:.2f}% - Val Loss: {val_loss:.4f} - Val AUC: {auc:.4f} - Val EER: {eer:.4f}")
 
         # Save the best model based on AUC
-        if (epoch+1) % save_every == 0 or auc > best_auc and auc < 1:   # exceptional case, auc =1
+        if (epoch+1) % save_every == 0 and auc > best_auc and auc < 1:   # exceptional case, auc =1
             best_auc = auc
             best_eer = eer
             save_path_epoch = f"{save_path}_epoch_{epoch + 1}.pth"  # Save with epoch info
@@ -189,10 +191,10 @@ def get_model(model_name, device):
         model = RawNet(parser1['model'], device).to(device)
     elif model_name.lower() == 'ssdnet':
         # SSDNet Model
-        model = SSDNet1D_L().to(device)    # SSDNet1D_S, DilatedNet_S, SSDNet1D_L, DilatedNet_L
+        model = SSDNet1D().to(device)    # SSDNet1D_S, DilatedNet_S, SSDNet1D_L, DilatedNet_L
     elif model_name.lower() == 'inc_ssdnet':
         # SSDNet Model
-        model = DilatedNet_L().to(device)    # SSDNet1D_S, DilatedNet_S, SSDNet1D_L, DilatedNet_L
+        model = DilatedNet().to(device)    # SSDNet1D_S, DilatedNet_S, SSDNet1D_L, DilatedNet_L
     elif model_name.lower() == 'resnet1d':
         # ResNet1D
         model = ResNet1D(in_channels = 1 , base_filters = 128, kernel_size = 5, stride=2, groups = 1, n_block = 3, n_classes = 2).to(device)
@@ -217,31 +219,39 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--root', '-rt', type=str, default='../DATASETS/DTIM', help='')
     parser.add_argument('--nEpochs', '-epoch', type=int, default=50, help='')
-    parser.add_argument('--batch_size', '-b', type=int, default=256, help='')
-    parser.add_argument('--num_workers', '-w', type=int, default=4, help='')
+    parser.add_argument('--batch_size', '-b', type=int, default=64, help='')
+    parser.add_argument('--num_workers', '-w', type=int, default=8, help='')
     parser.add_argument('--lr', '-lr', type=float, default=0.001, help='')
     # parser.add_argument("--gpu_devices", type=int, nargs='+', default=[0], help='')
     args = parser.parse_args()
     print(args)
 
-    args.ratio = 8
+    # args.ratio = 8
     # Example usage
-    train_dataset = RawNetDATAReader(args=args, split='TRAIN')
+
+    datasets_root = "/data/Shared_Audio/A_Datasets"   # Awais local PC: "/mnt/f/Awais_data/Datasets"
+    dataset_names = ["release_in_the_wild", "ASV_2019", "Halftruth"]
+    splits = ["train","eval"]
+    train_dataset = CombinedDATAReader(datasets_root, dataset_names)
+    test_dataset = train_dataset.get_test_dataset()
+
+    # train_dataset = RawNetDATAReader(args=args, split='TRAIN')
+    # dev_dataset = RawNetDATAReader(args=args, split='TEST')
     train_loader = data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers, pin_memory=True, drop_last=True)
+    dev_loader = data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers, pin_memory=True, drop_last=True)
 
-    dev_dataset = RawNetDATAReader(args=args, split='TEST')
-    dev_loader = data.DataLoader(dev_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers, pin_memory=True, drop_last=True)
+    model_list = ["rawnet3","rawnet2", "rawboost","resnet1d", "msresnet", "ssdnet", "inc_ssdnet", "ssdnet_small", "ssdnet_large", "inc_ssdnet_small", "inc_ssdnet_large" ]
 
-    model_name = "inc_ssdnet" # models_list = "aasist", "rawnet3", "rawnet2", "rawboost", "ssdnet", "inc_ssdnet", "resnet1d", "msresnet", "acnn1d"
-    save_every=10
-    device_id = 3
+    model_name = "resnet1d" # models_list = "aasist", "rawnet3", "rawnet2", "rawboost", "ssdnet", "inc_ssdnet", "resnet1d", "msresnet", "acnn1d"
+    save_every = 5
+    device_id = 7
     device = torch.device('cuda',device_id)
     model = get_model(model_name,device)
 
     os.makedirs(f"./weights/{model_name}",exist_ok=True)
-    model_save_path = f"./weights/{model_name}/best_{model_name}_ratio_{args.ratio}"
+    model_save_path = f"./weights/{model_name}/best_{model_name}_Combined"
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=args.lr)  # Only optimize the FC layer
-
+    print(f"Training {model_name}")
     best_model_path = train_model(model, criterion, optimizer , train_loader, dev_loader, num_epochs=args.nEpochs, save_path=model_save_path,save_every=save_every)
     print(f"Best model saved at: {best_model_path}")
